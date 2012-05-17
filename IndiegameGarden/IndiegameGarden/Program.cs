663b69630986e53bb580ec6a7dfc7a97a52c013a
TranceTrance <info@trancetrance.com>
2012-05-17 22:07:17 +0200

    added remote exception reporting function for beta testers

--- IndiegameGarden/IndiegameGarden/Program.cs
+++ IndiegameGarden/IndiegameGarden/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Windows.Forms;
 using Microsoft.Xna.Framework.Graphics;
 using TTengine.Util;
+using MyDownloader.Core;
 
 namespace IndiegameGarden
 {
@@ -25,8 +26,26 @@ namespace IndiegameGarden
                 }
             }
             catch(Exception ex) {
-                MessageBox.Show("Critical error - sorry! " + ex.Message + "\n" + ex.ToString(), "IndiegameGarden: critical error (well, I'm just an ALPHA version!)");
+                ReportErrorOverNetwork(ex);
+                MessageBox.Show("Critical error - sorry! " + ex.Message + "\n" + ex.ToString(), "IndiegameGarden: critical error (well, I'm just an ALPHA version!)");                
             }
+        }
+
+        static void ReportErrorOverNetwork(Exception ex) {
+            try
+            {
+                string stShort = ex.StackTrace;
+                if (stShort.Length > 200)
+                    stShort = stShort.Substring(0, 200);
+                string u = "http://indieget.appspot.com/err?ex=" + ex.Message + "&ts=" + ex.TargetSite + "&st=" + stShort;
+                Downloader downloader = DownloadManager.Instance.Add(ResourceLocation.FromURL(u), new ResourceLocation[] { },
+                                                "temp23048230948209348230894432.tmp", 1, true);
+            }
+            catch (Exception)
+            {
+                ;
+            }
+
         }
     }
 #endif
