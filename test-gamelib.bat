c19e903f43d0dce9fc953c121c738b5a39f00e34
TranceTrance <info@trancetrance.com>
2012-12-25 21:38:53 +0100

    added script for local gamelib playtesting

---
+++ test-gamelib.bat
@@ -0,0 +1,12 @@
+@echo off
+rem Puts .bin file in IGG folder for local test
+rem First builds it.
+
+call build-gamelib.bat
+
+cd config\gamelib_fmt%FORMATVER%
+copy /Y gamelib.bin "%USERPROFILE%\Local Settings\Application Data\IndiegameGarden\config\igg_gamelib_fmt%FORMATVER%\gamelib.bin"
+cd ..\..
+
+echo Ready for testing with local Indiegame Garden
+
