87a67c1818f0f014b307edd4c4c2cddffd882981
TranceTrance <info@trancetrance.com>
2013-09-29 21:30:38 +0200

    updated AssemblyInfo

--- IndiegameGarden/GameLibCompiler/Properties/AssemblyInfo.cs
+++ IndiegameGarden/GameLibCompiler/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GameLibCompiler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GameLibCompiler for Bento Box")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IndiegameGarden.com")]
 [assembly: AssemblyProduct("GameLibCompiler")]
-[assembly: AssemblyCopyright("Copyright © IndiegameGarden.com 2012")]
+[assembly: AssemblyCopyright("Copyright © IndiegameGarden.com 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("29a43766-6d41-4111-9d84-23bdef66b536")]
+[assembly: Guid("29a43876-6d41-4111-9a84-24bdef66b536")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
--- IndiegameGarden/IndiegameGarden/Properties/AssemblyInfo.cs
+++ IndiegameGarden/IndiegameGarden/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Glorious Wreckz Garden")]
-[assembly: AssemblyProduct("Glorious Wreckz Garden")]
-[assembly: AssemblyDescription("Glorious Wreckz Garden")]
+[assembly: AssemblyTitle("Bento Box")]
+[assembly: AssemblyProduct("Bento Box")]
+[assembly: AssemblyDescription("Bento Box - Have an indie game for lunch")]
 [assembly: AssemblyCompany("IndiegameGarden.com")]
 [assembly: AssemblyCopyright("Copyright © IndiegameGarden.com 2013")]
 [assembly: AssemblyTrademark("")]
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // On Windows, the following GUID is for the ID of the typelib if this
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
-[assembly: Guid("eb35aa52-cd6e-4384-bc1e-24e56e463ffb")]
+[assembly: Guid("eb46ab53-cd6e-4384-bc1e-24e56e463ffb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
